ef3d48a82d9425a08ae02eac72c604ec359d7cfe
ISHANK GAMU <68808097+ishankgamu-dev@users.noreply.github.com>
2020-08-01 23:45:17 +0530

    Update uptime.js

--- uptime.js
+++ uptime.js
@@ -1,1 +1,19 @@
+let express = require("express"),
+    http = require('http'),
+    app = express();
 
+app.use(express.static("public"));
+app.get("/", function(request, response) {
+  response.sendStatus(200); // Status: OK
+});
+
+let listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
+
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+//don't need to fill anything on process.env.PROJECT_DOMAIN or process.env.PORT
+//wait 5 minutes of forward : )
