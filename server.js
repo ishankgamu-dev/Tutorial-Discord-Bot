188b01b8ba2640cf51ce5c6f39133dfb83751792
ISHANK GAMU <68808097+ishankgamu-dev@users.noreply.github.com>
2020-08-01 23:50:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+const Discord =require('discord.js);
+const client =new Discord.Client();
 
+
+client.on('message', async message => {
+   if (!message.content.startsWith(prefix)) return; // use this only, so your bot will be only executed with prefix.
+  
+  let args = message.content.slice(prefix.length).trim().split(/ +/g);
+  let msg = message.content.toLowerCase();
+  let cmd = args.shift().toLowerCase();
+  
+  message.flags = [];
+  while (args[0] && args[0][0] === "-") {
+    message.flags.push(args.shift().slice(1)); // Message Flags: -default, -ban, -parameter...
+  }
+  
